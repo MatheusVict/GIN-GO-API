02731fc3b2ff5ec41da98df39a81b35853820d82
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-18 12:53:11 -0300

    chore: adding dotenv lib
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error on .env file")
+	}
+	fmt.Println(os.Getenv("TEST"))
+}
