d87518152ec9441d8417a78c6b0a0594283e3fe2
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-18 16:20:07 -0300

    feat: user request and response
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/MatheusVict/User-Register-GO/src/configuration/errorsHandle"
+	"github.com/MatheusVict/User-Register-GO/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func CreateUser(ctx *gin.Context) {
-	err := errorsHandle.NewBadRequestError("Wrong path")
-	ctx.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := ctx.ShouldBindJSON(&userRequest); err != nil {
+		restErr := errorsHandle.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()),
+		)
+		ctx.JSON(restErr.Code, restErr)
+		return
+	}
+
+	log.Println(userRequest)
 }
