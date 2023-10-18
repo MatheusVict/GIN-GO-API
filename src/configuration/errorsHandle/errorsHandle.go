34f3335f85926fff539a87f8ca5f9059332643f9
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-18 13:22:42 -0300

    feat: error handler
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/configuration/errorsHandle/errorsHandle.go
@@ -0,0 +1,52 @@
+package errorsHandle
+
+import "net/http"
+
+type ErrorsHandle struct {
+	Message string   `json:"message"`
+	Err     string   `json:"error"`
+	Code    int      `json:"code"`
+	Causes  []Causes `json:"causes,omitempty"`
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func (e *ErrorsHandle) Error() string {
+	return e.Message
+}
+
+func NewErrorsHandle(message string, err string, code int, causes []Causes) *ErrorsHandle {
+	return &ErrorsHandle{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestError(message string) *ErrorsHandle {
+	return &ErrorsHandle{
+		Message: message,
+		Err:     "Bad request",
+		Code:    http.StatusBadRequest,
+	}
+}
+func NewBadRequestValidationError(message string, causes []Causes) *ErrorsHandle {
+	return &ErrorsHandle{
+		Message: message,
+		Err:     "Bad request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *ErrorsHandle {
+	return &ErrorsHandle{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
