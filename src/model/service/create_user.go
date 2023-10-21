b65ae06d2263815fc4b9c8944c17af39cfeda864
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-21 16:19:39 -0300

    fix: validation user email
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -10,6 +10,13 @@ func (u *userDomainService) CreateUserService(
 	userDomain model.UserDomainInterface,
 ) (model.UserDomainInterface, *errorsHandle.ErrorsHandle) {
 	log.Println("CreateUser")
+	user, err := u.FindUserByEmailService(userDomain.GetEmail())
+	if user != nil {
+		return nil, errorsHandle.NewBadRequestError("Email is already registered in another account")
+	}
+	if err != nil {
+		log.Println("Error on search user")
+	}
 	userDomain.EncryptPassword()
 	log.Println(userDomain.GetPassword())
 
